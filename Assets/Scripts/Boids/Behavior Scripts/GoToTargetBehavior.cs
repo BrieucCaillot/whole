44c9249dc3c0f9052bf9980f0f7255a334d95b63
Sergio335566 <bocanceaserge335566@gmail.com>
2020-04-03 16:20:38 +0200

    init

---
+++ Assets/Scripts/Boids/Behavior Scripts/GoToTargetBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Target")]
+public class GoToTargetBehavior : FlockBehavior
+{
+    Vector3 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+
+    public Vector3 targetPosition = Random.insideUnitSphere * 50;
+
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        Vector3 targetMove = targetPosition - agent.transform.position;
+
+        // targetMove = Vector3.SmoothDamp(agent.transform.forward, targetMove, ref currentVelocity, agentSmoothTime);
+
+        return targetMove;
+    }
+}
---
+++ Assets/Scripts/Boids/Behavior Scripts/SteeredAlignmentBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/SteeredAlignment")]
+public class SteeredAlignmentBehavior : FilterFlockBehavior
+{
+    Vector3 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //if no neigbors, maintain current alignment
+        if (context.Count == 0) {
+            return agent.transform.forward;   
+        }
+
+        Vector3 alignmentMove = Vector3.zero;
+
+        List<Transform> filteredContext = (filter == null) ? context : filter.Filter(agent, context);
+        foreach (Transform item in filteredContext)
+        {
+            alignmentMove += item.transform.forward;
+        }
+
+        alignmentMove /= context.Count;
+        
+        alignmentMove = Vector3.SmoothDamp(agent.transform.forward, alignmentMove, ref currentVelocity, agentSmoothTime);
+
+        return alignmentMove;
+    }
+}
