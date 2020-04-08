03954841ba7846a716a00802adfd9ad18ccd2651
Sergio335566 <serge.bocancea@laposte.net>
2020-04-08 14:24:01 +0200

    setup Sky birds / ocean

---
+++ Assets/Scripts/Sea/Boids/Behavior Scripts/StayInRadiusBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Flock/Behavior/StayInRadius")]
+public class StayInRadiusBehavior : FlockBehavior
+{
+    public Vector3 center;
+    public float radius = 100;
+
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        Vector3 move = Vector3.zero;
+        Vector3 centerOffset = center - agent.transform.position;
+        float t = centerOffset.magnitude / radius;
+        if (t < 0.9f)
+        {
+            move = Vector3.zero;
+        }
+        else
+        {
+            move = centerOffset * t * t;
+        }
+
+        return move;
+    }
+}
---
+++ Assets/Scripts/Sky/BirdsBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsBehavior : MonoBehaviour
+{
+    private Transform parent;
+    [SerializeField] private GameObject birdPreFab;
+    [SerializeField] private int numOfBirds = 5;
+    [SerializeField] private GameObject CenterMountain;
+
+    private List<GameObject> allBirds = new List<GameObject>();
+    void Start()
+    {
+        parent = transform;
+        Vector3 parentPosition = parent.transform.position;
+        
+        for (int i = 0; i < numOfBirds; i++)
+        {
+            Vector3 randomBirdPosition = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
+           allBirds.Add((GameObject) Instantiate(birdPreFab, parent.transform.position + randomBirdPosition, Quaternion.identity, parent));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < allBirds.Count; i++)
+        {
+            // allBirds[i].transform.position = Vector3.Slerp(parent.transform.position + randomBirdPosition);
+            allBirds[i].transform.LookAt(CenterMountain.transform.position);
+        }
+    }
+}
