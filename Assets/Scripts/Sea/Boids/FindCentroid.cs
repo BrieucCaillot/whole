3855a798c257ae266181ae600feb122bf91b4af2
Sergio335566 <serge.bocancea@laposte.net>
2020-04-08 14:24:01 +0200

    setup Sky birds / ocean

---
+++ Assets/Scripts/Sea/Boids/FindCentroid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ReturnCentroid
+{
+    public static Vector3 FindCentroid ( List< FlockAgent > targets ) {
+ 
+         Vector3 centroid;
+         Vector3 minPoint = targets[ 0 ].transform.position;
+         Vector3 maxPoint = targets[ 0 ].transform.position;
+ 
+         for ( int i = 1; i < targets.Count; i ++ ) {
+             Vector3 pos = targets[ i ].transform.position;
+             if( pos.x < minPoint.x )
+                 minPoint.x = pos.x;
+             if( pos.x > maxPoint.x )
+                 maxPoint.x = pos.x;
+             if( pos.y < minPoint.y )
+                 minPoint.y = pos.y;
+             if( pos.y > maxPoint.y )
+                 maxPoint.y = pos.y;
+             if( pos.z < minPoint.z )
+                 minPoint.z = pos.z;
+             if( pos.z > maxPoint.z )
+                 maxPoint.z = pos.z;
+         }
+ 
+         centroid = minPoint + 0.5f * ( maxPoint - minPoint );
+ 
+         return centroid;
+
+     }
+}
---
+++ Assets/Scripts/Sky/BezierRoutes/Route.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Route : MonoBehaviour
+{
+   [SerializeField] private Transform[] controlPoints;
+   private Vector3 gizmosPositions;
+
+
+    private void OnDrawGizmos() 
+    {
+        for (float i = 0; i <= 1; i += 0.05f)
+        {
+            gizmosPositions = Mathf.Pow(1-i, 3) * 
+                            controlPoints[0].position +
+                                3 * Mathf.Pow(1-i, 2) * i * 
+                            controlPoints[1].position +
+                                3 * (1-i) * Mathf.Pow(i, 2) *
+                            controlPoints[2].position + 
+                                Mathf.Pow(i, 3) * 
+                            controlPoints[3].position;
+            Gizmos.DrawSphere(gizmosPositions, 0.15f);
+        }
+        Gizmos.DrawLine(
+        new Vector3(controlPoints[0].position.x, controlPoints[0].position.y, controlPoints[0].position.z),
+        new Vector3(controlPoints[1].position.x, controlPoints[1].position.y, controlPoints[1].position.z));
+        Gizmos.DrawLine(
+        new Vector3(controlPoints[2].position.x, controlPoints[2].position.y, controlPoints[2].position.z),
+        new Vector3(controlPoints[3].position.x, controlPoints[3].position.y, controlPoints[3].position.z));
+    }
+}
