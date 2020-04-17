2f508fe9956bbe52942e9dad6a51e5266a336398
BrieucKyo <brieucdeveloper@gmail.com>
2020-04-17 18:09:50 +0200

    Audio with subtitle done

---
+++ Assets/Scripts/Controllers/UIController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+
+    public AudioController _audioController;
+    public SubtitleController _subtitleController;
+    
+    private int interactionNumber = 0;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {    
+            PlayUI();
+        }
+    }
+
+    public void PlayUI()
+    {
+        if (!_audioController.getActive() && !_subtitleController.getActive())
+        {
+            interactionNumber++;
+            _audioController.PlayAudio(interactionNumber);
+            _subtitleController.GetSubtitles(interactionNumber);
+        }
+    }
+}
