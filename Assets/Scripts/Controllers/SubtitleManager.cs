a56f4fd0df9a788aba4c4bef4d17cf0c9d24aaed
BrieucKyo <brieucdeveloper@gmail.com>
2020-04-20 20:00:34 +0200

    Play ambiant sound

---
+++ Assets/Scripts/Controllers/SubtitleManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable] 
+public class SubtitleManager : MonoBehaviour
+{
+    private Text subtitleContent;
+    private Array allSubtitles;
+    private string path;
+    private string jsonString;
+    private bool isActive = false;
+
+    public void GetSubtitles(int interactionNumber)
+    {
+        if (!isActive)
+        {
+            GetSubtitlesFromJson(interactionNumber);
+            StartCoroutine(DisplaySubtitles());
+        }
+    }
+
+    private void GetSubtitlesFromJson(int interactionNumber)
+    {
+        isActive = true;
+        try
+        {
+            path = Application.streamingAssetsPath + "/Subtitles/SubtitlesInteraction" + interactionNumber + ".json";
+            jsonString = File.ReadAllText(path);
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.Log(e);
+            gameObject.GetComponent<Text>().text = "No more subtitles for this interaction";
+            return;
+        }
+        
+        Subtitles subtitlesInJson = JsonUtility.FromJson<Subtitles>(jsonString);
+        allSubtitles = subtitlesInJson.subtitles;
+    }
+
+    IEnumerator DisplaySubtitles()
+    {
+        int index = 0;
+
+        foreach (Subtitle subtitle in allSubtitles)
+        {
+            gameObject.GetComponent<Text>().text = subtitle.content;
+            yield return new WaitForSeconds(subtitle.duration);
+            
+            if (index == allSubtitles.Length - 1)
+            {
+                gameObject.GetComponent<Text>().text = "";
+                allSubtitles = null;
+                isActive = false;
+            }
+
+            index++;
+        }
+    }
+    
+    public bool GetActive()
+    {
+        return isActive;
+    }
+}
+
+
+[System.Serializable] 
+public class Subtitles
+{
+    public Subtitle[] subtitles;
+}
+
+
+[System.Serializable] 
+public class Subtitle
+{
+    public string content;
+    public int duration;
+}
---
+++ Assets/Scripts/Controllers/VoiceoverManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class VoiceoverManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private AudioClip audioClip;
+    private string path;
+    private string audioName;
+
+    private void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        path = "file://" + Application.streamingAssetsPath + "/Audio/Voiceover/";
+    }
+
+    public void PlayVoiceover(int interactionNumber)
+    {
+        if (!GetActive())
+        {
+            StartCoroutine(LoadAudio(interactionNumber));
+        }
+    }
+    
+    private WWW GetAudioFromFile(string path, string fileName)
+    {
+        string audioToLoad = path + fileName;
+        WWW request = new WWW(audioToLoad);
+        return request;
+    }
+
+    private IEnumerator LoadAudio(int interactionNumber)
+    {
+        audioName = "Voiceover" + interactionNumber + ".wav";
+        
+        WWW request = GetAudioFromFile(path, audioName);
+        yield return request;
+
+        audioClip = request.GetAudioClip();
+        audioClip.name = audioName;
+        
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+    public bool GetActive()
+    {
+        return audioSource.isPlaying;
+    }
+}
