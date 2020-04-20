895e5dbce5017ab9f54afc3ee1acc9393405dbbb
BrieucKyo <brieucdeveloper@gmail.com>
2020-04-20 20:00:34 +0200

    Play ambiant sound

---
+++ Assets/Scripts/Controllers/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private AudioClip audioClip;
+    private string path;
+    private string audioName;
+
+    private void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        path = "file://" + Application.streamingAssetsPath + "/Audio/Sounds/";
+    }
+
+    public void PlaySound(string name)
+    {
+        if (!GetActive())
+        {
+            StartCoroutine(LoadAudio(name));
+        }
+    }
+
+    public void PauseSound()
+    {
+        audioSource.Pause();
+    }
+    
+    public void UnPauseSound()
+    {
+        audioSource.UnPause();
+    }
+    
+    private WWW GetAudioFromFile(string path, string fileName)
+    {
+        string audioToLoad = path + fileName;
+        WWW request = new WWW(audioToLoad);
+        return request;
+    }
+
+    private IEnumerator LoadAudio(string name)
+    {
+        audioName = name + ".wav";
+        
+        WWW request = GetAudioFromFile(path, audioName);
+        yield return request;
+
+        audioClip = request.GetAudioClip();
+        audioClip.name = audioName;
+
+        audioSource.loop = true;
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+    public bool GetActive()
+    {
+        return audioSource.isPlaying;
+    }
+}
--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -3,27 +3,50 @@
 public class UIController : MonoBehaviour
 {
 
-    public AudioController _audioController;
-    public SubtitleController _subtitleController;
+    public AudioManager _AudioManager;
+    public VoiceoverManager _VoiceoverManager;
+    public SubtitleManager _SubtitleManager;
     
     private int interactionNumber = 0;
     
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {    
-            PlayUI();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayVoiceoverAndSubtitles();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _AudioManager.PauseSound();
+            _AudioManager.PlaySound("Weather/Wind");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            _AudioManager.PauseSound();
+            _AudioManager.PlaySound("Water/Underwater");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            _AudioManager.PauseSound();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            _AudioManager.UnPauseSound();
         }
     }
 
-    public void PlayUI()
+    public void PlayVoiceoverAndSubtitles()
     {
-        if (!_audioController.getActive() && !_subtitleController.getActive())
+        if (!_VoiceoverManager.GetActive() && !_SubtitleManager.GetActive())
         {
             interactionNumber++;
-            _audioController.PlayAudio(interactionNumber);
-            _subtitleController.GetSubtitles(interactionNumber);
+            _VoiceoverManager.PlayVoiceover(interactionNumber);
+            _SubtitleManager.GetSubtitles(interactionNumber);
         }
     }
 }
