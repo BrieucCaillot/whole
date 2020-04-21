d215046dda371a2a3e9b22bcc9772725be281b19
leochocolat <l.mouraire@outlook.fr>
2020-04-21 14:36:21 +0200

    make boids working without using colliders

--- Assets/Scripts/Sea/Boids/Flock.cs
+++ Assets/Scripts/Sea/Boids/Flock.cs
@@ -11,6 +11,9 @@ public class Flock : MonoBehaviour
     public FlockBehavior behavior;
 
     [Range(10, 500)]
+    public int containerSize = 200;
+
+    [Range(10, 500)]
     public int startingCount = 200;
     // const float AgentDensity = 0.5f;
 
@@ -23,8 +26,8 @@ public class Flock : MonoBehaviour
     [Range(1.0f, 50.0f)]
     public float neighborRadius = 10.0f;
 
-    [Range(0.0f, 5.0f)]
-    public float avoidanceRadiusMultiplier = 0.5f;
+    [Range(0.0f, 1.0f)]
+    public float avoidanceRadiusMultiplier = 0.1f;
 
     float squareMaxSpeed;
     float squareNeighborRadius;
@@ -36,16 +39,15 @@ public class Flock : MonoBehaviour
     {
         squareMaxSpeed = maxSpeed * maxSpeed;
         squareNeighborRadius = neighborRadius * neighborRadius;
-        //TODO: Find which one is best ?
-        // squareAvoidanceRadius = squareNeighborRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
-        squareAvoidanceRadius = squareNeighborRadius * squareNeighborRadius * avoidanceRadiusMultiplier;
+        squareAvoidanceRadius = (neighborRadius * avoidanceRadiusMultiplier) * (neighborRadius * avoidanceRadiusMultiplier);
 
         for (int i = 0; i < startingCount; i++)
         {
             FlockAgent newAgent = Instantiate(
                 agentPrefab,
-                Random.insideUnitSphere * 50,
-                Quaternion.identity,
+                Random.insideUnitSphere * containerSize,
+                // Quaternion.identity,
+                Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),
                 transform
             );
 
@@ -63,6 +65,8 @@ public class Flock : MonoBehaviour
             List<Transform> context = GetNearbyObjects(agent);
             Vector3 move = behavior.CalculateMove(agent, context, this);
             move *= driveFactor;
+
+
             if (move.sqrMagnitude > squareMaxSpeed)
             {
                 move = move.normalized * maxSpeed;
@@ -75,13 +79,17 @@ public class Flock : MonoBehaviour
     List<Transform> GetNearbyObjects(FlockAgent agent)
     {
         List<Transform> context = new List<Transform>();
-        Collider[] contextColliders = Physics.OverlapSphere(agent.transform.position, neighborRadius);
 
-        foreach (Collider collider in contextColliders)
+        foreach (FlockAgent item in agents)
         {
-            if (collider != agent.AgentCollider)
+            if (item != agent)
             {
-                context.Add(collider.transform);
+                Vector3 heading = item.AgentPosition - agent.AgentPosition;
+
+                if (heading.sqrMagnitude < squareNeighborRadius)
+                {
+                    context.Add(item.AgentTransform);
+                }
             }
         }
 
