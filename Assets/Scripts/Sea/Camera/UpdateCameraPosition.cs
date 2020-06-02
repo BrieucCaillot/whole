ae0106adf24c0864940f59740566e64e52c75caa
leochocolat <l.mouraire@outlook.fr>
2020-06-03 00:12:00 +0200

    resetup camera target on boids average position

--- Assets/Scripts/Sea/Camera/UpdateCameraPosition.cs
+++ Assets/Scripts/Sea/Camera/UpdateCameraPosition.cs
@@ -14,7 +14,34 @@ public class UpdateCameraPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // transform.position = (ReturnCentroid.FindCentroid(BoidManager.agentsPositions));
-        // Debug.Log(ReturnCentroid.FindCentroid(Flock.agents));
+        transform.position = FindCentroid(BoidManager.agentsPositions);
     }
+
+    Vector3 FindCentroid ( List<Vector3> agentsPositions ) {
+ 
+         Vector3 centroid;
+         Vector3 minPoint = agentsPositions[0];
+         Vector3 maxPoint = agentsPositions[0];
+ 
+         for ( int i = 1; i < agentsPositions.Count; i ++ ) {
+             Vector3 pos = agentsPositions[i];
+             if( pos.x < minPoint.x )
+                 minPoint.x = pos.x;
+             if( pos.x > maxPoint.x )
+                 maxPoint.x = pos.x;
+             if( pos.y < minPoint.y )
+                 minPoint.y = pos.y;
+             if( pos.y > maxPoint.y )
+                 maxPoint.y = pos.y;
+             if( pos.z < minPoint.z )
+                 minPoint.z = pos.z;
+             if( pos.z > maxPoint.z )
+                 maxPoint.z = pos.z;
+         }
+ 
+         centroid = minPoint + 0.5f * ( maxPoint - minPoint );
+ 
+         return centroid;
+
+     }
 }
