fc7ef92f0a4106c070620fa7c236784ba11c67f7
leochocolat <l.mouraire@outlook.fr>
2020-06-12 14:25:59 +0200

    add default interaction

---
+++ Assets/Scripts/interactions/Scripts/DefaultInteraction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DefaultInteraction : Interaction
+{
+    private bool go = false;
+
+    void Start()
+    {
+        Invoke("Go", 2f);
+        Debug.Log(go);
+
+    }
+
+    void Go()
+    {
+        go = true;
+        Debug.Log(go);
+    }
+
+    public override string GetAction() {
+        return action;
+    }
+
+    public override bool Listen()
+    {
+        return go;
+    }
+}
